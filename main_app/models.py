862aeccf34f47b89c2295775c932991fc4dc5c73
Anthony Martinez <martinezanthony213@gmail.com>
2020-12-17 14:32:57 -0800

    Connects and Uses Database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    year = models.IntegerField()
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    engine = models.CharField(max_length=200)
+    vin = models.IntegerField()
+    def __str__(self):
+        return self.model
